062de4de631c386e85f694d7dd56ad33d1e13691
Kimwel Beller <94892701+Kheemwel@users.noreply.github.com>
2023-09-19 20:41:17 +0800

    remove redundant links

--- resources/views/livewire/users/students/students-livewire.blade.php
+++ resources/views/livewire/users/students/students-livewire.blade.php
@@ -1,21 +1,6 @@
 @section('head')
     <title>Admin | Students</title>
 
-    <!--Dropdown Button Bootstrap-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Google Font: Source Sans Pro -->
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback" rel="stylesheet">
-    <!-- Font Awesome -->
-    <link href="adminLTE-3.2/plugins/fontawesome-free/css/all.min.css" rel="stylesheet">
-    <!-- overlayScrollbars -->
-    <link href="adminLTE-3.2/plugins/overlayScrollbars/css/OverlayScrollbars.min.css" rel="stylesheet">
-    <!-- Toastr -->
-    <link href="adminLTE-3.2/plugins/toastr/toastr.min.css" rel="stylesheet">
-    <!--iconify icons-->
-    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
-    <!-- Theme style -->
-    <link href="adminLTE-3.2/dist/css/adminlte.min.css" rel="stylesheet">
-
     <style>
         /* For Eye Icons of Anecdotal and Summary Section inside the table */
         .btn-primary.action-btn {
@@ -230,18 +215,35 @@
 </div> <!-- /.card-body -->
 
 @section('scripts')
-    <!-- jQuery -->
-    <script src="adminLTE-3.2/plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="adminLTE-3.2/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- overlayScrollbars -->
-    <script src="adminLTE-3.2/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="adminLTE-3.2/dist/js/adminlte.min.js"></script>
-    <!-- bs-custom-file-input -->
-    <script src="adminLTE-3.2/plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
-    <!-- Toastr -->
-    <script src="adminLTE-3.2/plugins/toastr/toastr.min.js"></script>
-    <!-- ChartJS -->
-    <script src="adminLTE-3.2/plugins/chart.js/Chart.min.js"></script>
+    <script>
+        $(function() {
+            var donutData = {
+                labels: [
+                    'Verbal Offense',
+                    'Physical Offense',
+                    'Social Media Offense'
+                ],
+                datasets: [{
+                    data: [800, 500, 200],
+                    backgroundColor: ['#0A0863', '#7684B9', '#F5C91A'],
+                }]
+            }
+            //- PIE CHART -
+            //-------------
+            // Get context with jQuery - using jQuery's .get() method.
+            var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+            var pieData = donutData;
+            var pieOptions = {
+                maintainAspectRatio: false,
+                responsive: true,
+            }
+            //Create pie or douhnut chart
+            // You can switch between pie and douhnut using the method below.
+            new Chart(pieChartCanvas, {
+                type: 'pie',
+                data: pieData,
+                options: pieOptions
+            })
+        })
+    </script>
 @endsection
