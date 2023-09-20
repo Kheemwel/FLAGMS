5b885a8b97440b717e14e42e050268c5f1d69efd
Kimwel Beller <94892701+Kheemwel@users.noreply.github.com>
2023-09-20 15:41:37 +0800

    make the column type  from string to mediumtext

--- database/migrations/2023_09_13_200625_create_website_titles_table.php
+++ database/migrations/2023_09_13_200625_create_website_titles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('website_title', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->mediumText('title');
             $table->timestamps();
         });
 
--- database/migrations/2023_09_14_214101_create_website_subtitles_table.php
+++ database/migrations/2023_09_14_214101_create_website_subtitles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('website_subtitle', function (Blueprint $table) {
             $table->id();
-            $table->string('subtitle');
+            $table->mediumText('subtitle');
             $table->timestamps();
         });
 
