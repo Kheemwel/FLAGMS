153e13ffc07ffb27ed93f17105015dd0137ce345
Kimwel Beller <94892701+Kheemwel@users.noreply.github.com>
2023-10-01 16:59:37 +0800

    fix offenses page tabs state

--- resources/views/livewire/file_management/offenses/offenses-livewire.blade.php
+++ resources/views/livewire/file_management/offenses/offenses-livewire.blade.php
@@ -2,9 +2,9 @@
     <title>Admin | Offenses</title>
 @endsection
 
-{{-- <div class="content-wrapper" style="background-color:  rgb(253, 253, 253); padding-left: 2rem;"> --}}
-<!-- Content Header (Page header) -->
-{{-- <section class="content-header">
+<div class="content-wrapper" style="background-color:  rgb(253, 253, 253); padding-left: 2rem;">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6" style="padding-left: 2rem; padding-top: 1rem;">
@@ -13,45 +13,46 @@
             </div>
         </div>
         <!-- /.container-fluid -->
-    </section> --}}
+    </section>
 
-<div class="card card-primary card-tabs content-wrapper" style="background-color:  rgb(253, 253, 253); padding-left: 2rem;">
-    <div class="card-header p-0 pt-1" style="background-color: #7684B9 !important">
-        <ul class="nav nav-tabs" id="custom-tabs-one-tab" role="tablist">
-            <li class="nav-item">
-                <a aria-controls="custom-tabs-one-offenses" aria-selected="true" class="nav-link active" data-toggle="pill" href="#custom-tabs-one-offenses" id="custom-tabs-one-offenses-tab" role="tab">
-                    <h3 style="font-weight: bold;">Offenses</h3>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a aria-controls="custom-tabs-one-categories" aria-selected="false" class="nav-link" data-toggle="pill" href="#custom-tabs-one-categories" id="custom-tabs-one-categories-tab" role="tab">
-                    <h3 style="font-weight: bold;">Categories</h3>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a aria-controls="custom-tabs-one-sanctions" aria-selected="false" class="nav-link" data-toggle="pill" href="#custom-tabs-one-sanctions" id="custom-tabs-one-sanctions-tab" role="tab">
-                    <h3 style="font-weight: bold;">Sanctions</h3>
-                </a>
-            </li>
-        </ul>
-    </div>
-    <div class="card-body" wire:ignore.self>
-        <div class="tab-content" id="custom-tabs-one-tabContent" style="padding-right: 2rem;">
-            <div aria-labelledby="custom-tabs-one-offense-tab" class="tab-pane fade active show" id="custom-tabs-one-offenses" role="tabpanel">
-                @include('livewire.file_management.offenses.offenses-table')
-            </div>
-            <div aria-labelledby="custom-tabs-one-categories-tab" class="tab-pane fade" id="custom-tabs-one-categories" role="tabpanel">
-                @include('livewire.file_management.offenses.offenses-categories')
-            </div>
-            <div aria-labelledby="custom-tabs-one-sanctions-tab" class="tab-pane fade" id="custom-tabs-one-sanctions" role="tabpanel">
-                @include('livewire.file_management.offenses.offenses-sanctions')
+    <div class="card card-primary card-tabs " style="background-color:  rgb(253, 253, 253); margin-left: 2rem; margin-right: 2rem;">
+        <div class="card-header p-0 pt-1" style="background-color: #7684B9 !important">
+            <ul class="nav nav-tabs" id="custom-tabs-one-tab" role="tablist">
+                <li class="nav-item" wire:ignore>
+                    <a aria-controls="custom-tabs-one-offenses" aria-selected="true" class="nav-link active" data-toggle="pill" href="#custom-tabs-one-offenses" id="custom-tabs-one-offenses-tab" role="tab">
+                        <h5 style="font-weight: bold;">Offenses</h5>
+                    </a>
+                </li>
+                <li class="nav-item" wire:ignore>
+                    <a aria-controls="custom-tabs-one-categories" aria-selected="false" class="nav-link" data-toggle="pill" href="#custom-tabs-one-categories" id="custom-tabs-one-categories-tab" role="tab">
+                        <h5 style="font-weight: bold;">Categories</h5>
+                    </a>
+                </li>
+                <li class="nav-item" wire:ignore>
+                    <a aria-controls="custom-tabs-one-sanctions" aria-selected="false" class="nav-link" data-toggle="pill" href="#custom-tabs-one-sanctions" id="custom-tabs-one-sanctions-tab" role="tab">
+                        <h5 style="font-weight: bold;">Sanctions</h5>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="card-body">
+            <div class="tab-content" id="custom-tabs-one-tabContent" style="padding-right: 2rem;">
+                <div aria-labelledby="custom-tabs-one-offense-tab" class="tab-pane fade active show" id="custom-tabs-one-offenses" role="tabpanel" wire:ignore.self>
+                    @include('livewire.file_management.offenses.offenses-table')
+                </div>
+                <div aria-labelledby="custom-tabs-one-categories-tab" class="tab-pane fade" id="custom-tabs-one-categories" role="tabpanel" wire:ignore.self>
+                    @include('livewire.file_management.offenses.offenses-categories')
+                </div>
+                <div aria-labelledby="custom-tabs-one-sanctions-tab" class="tab-pane fade" id="custom-tabs-one-sanctions" role="tabpanel" wire:ignore.self>
+                    @include('livewire.file_management.offenses.offenses-sanctions')
+                </div>
             </div>
         </div>
-    </div>
-    <!-- /.card -->
+        <!-- /.card -->
 
-    
-@include('livewire.file_management.offenses.add-offenses')
-@include('livewire.file_management.offenses.add-offenses-category')
-@include('livewire.file_management.offenses.add-sanctions')
+
+        @include('livewire.file_management.offenses.add-offenses')
+        @include('livewire.file_management.offenses.add-offenses-category')
+        @include('livewire.file_management.offenses.add-sanctions')
+    </div>
 </div>
