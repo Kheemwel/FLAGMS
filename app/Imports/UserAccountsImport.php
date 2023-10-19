b5dd609342e1db47c97da1bd948c3fdbd8823497
Kimwel Beller <94892701+Kheemwel@users.noreply.github.com>
2023-10-19 20:28:17 +0800

    batch add students, parents, principals, and teachers

--- app/Imports/UserAccountsImport.php
+++ app/Imports/UserAccountsImport.php
@@ -3,6 +3,10 @@
 namespace App\Imports;
 
 use App\Mail\AccountCreationMail;
+use App\Models\Parents;
+use App\Models\Principals;
+use App\Models\Students;
+use App\Models\Teachers;
 use App\Models\UserAccounts;
 use App\Traits\Toasts;
 use Illuminate\Support\Facades\Mail;
@@ -26,7 +30,18 @@ class UserAccountsImport implements ToModel, WithBatchInserts, WithProgressBar
     {
         $username = $this->checkUsername($row[2], $row[0], $row[1]);
         $password = $row[3] ? $row[3] : generatePassword();
-        $user = new UserAccounts([
+        // $idsArray = array_map('intval', explode(',', $row[8])); // fetch list of id in a row/cell
+        // $user = new UserAccounts([
+        //     'first_name' => $row[0],
+        //     'last_name' => $row[1],
+        //     'username' => $username,
+        //     'password' => $password,
+        //     'hashed_password' => bcrypt($password), // You can hash the password here
+        //     'role_id' => $row[4],
+        //     'email' => $row[5]
+        // ]);
+
+        $user = UserAccounts::create([
             'first_name' => $row[0],
             'last_name' => $row[1],
             'username' => $username,
@@ -36,6 +51,42 @@ class UserAccountsImport implements ToModel, WithBatchInserts, WithProgressBar
             'email' => $row[5]
         ]);
 
+        if ($user->getRole->role === 'Student') {
+            $school_level_id = $row[6];
+            $grade_level_id = $row[7];
+ 
+            Students::create([
+                'user_account_id' => $user->id,
+                'school_level_id' => $school_level_id,
+                'grade_level_id' => $grade_level_id,
+            ]);
+        }
+
+        
+        if ($user->getRole->role === 'Parent') {
+            $parent = Parents::create([
+                'user_account_id' => $user->id
+            ]);
+            
+            $ids = explode(',', str_replace(' ', '', $row[6]));
+            $parent->children()->attach($ids);
+        }
+
+        if ($user->getRole->role === 'Principal') {
+            $position_id = $row[6];
+            Principals::create([
+                'user_account_id' => $user->id,
+                'principal_position_id' => $position_id
+            ]);
+        }
+
+        
+        if ($user->getRole->role === 'Teacher') {
+            Teachers::create([
+                'user_account_id' => $user->id
+            ]);
+        }
+
         if ($user->email) {
             try {
                 Mail::to($user->email)->send(new AccountCreationMail($user->username, $user->password));
@@ -43,7 +94,6 @@ class UserAccountsImport implements ToModel, WithBatchInserts, WithProgressBar
                 //
             }
         }
-        return $user;
     }
 
     public function checkUsername($username, $firstname, $lastname)
