36e4e7bd491ef1e57f6e4181c3ce6a486e9ae1b1
Valerie Dela Cruz <m4nicsylla@gmail.com>
2023-11-18 11:21:55 +0700

    accounts table enhanced

--- resources/views/livewire/user_accounts/accounts-table.blade.php
+++ resources/views/livewire/user_accounts/accounts-table.blade.php
@@ -1,28 +1,58 @@
 <div x-data='{ rows: {} }'>
     <div style="margin-left: 2rem; margin-right: 2rem;">
         <div class="row">
-            <label for="per-page" style="font-weight: normal; margin-top: 1rem;">
-                Show
-                <span>
-                    <select class="form-select form-select-sm mb-2" id='per-page' selected wire:model.live.debounce.500ms="per_page">
-                        <option>10</option>
-                        <option>15</option>
-                        <option>20</option>
-                        <option>25</option>
-                        <option selected>30</option>
-                        <option>50</option>
-                        <option>100</option>
-                    </select>
-                </span>
-                Entries
-            </label>
-            <label for="per-page" style="font-weight: normal; margin-top: 1rem; margin-left: 1rem; cursor: pointer;" wire:click='markArchive(Object.keys(rows).filter(key => rows[key] === true))' x-show='Object.values(rows).includes(true)'>
-                <span class="archivals" style="transition: color 0.3s;">Mark as Archive</span>
-            </label>
+            <div class="col-2">
+                <label for="per-page" style="font-weight: normal; margin-top: 1rem;">
+                    Show
+                    <span>
+                        <select class="form-select form-select-sm mb-2" id='per-page' selected
+                            wire:model.live.debounce.500ms="per_page">
+                            <option>10</option>
+                            <option>15</option>
+                            <option>20</option>
+                            <option>25</option>
+                            <option selected>30</option>
+                            <option>50</option>
+                            <option>100</option>
+                        </select>
+                    </span>
+                    Entries
+                </label>
+            </div>
+
+            <div class='col-8 d-flex justify-content-end'>
+                <div class="input-group input-group-sm" style="width: 50%;">
+                    <input class="form-control float-right" name="table_search" placeholder="Search"
+                        style="height: 35px;" type="text" wire:model.live.debounce.500ms.debounce.500ms='search'>
+                    <div class="input-group-append">
+                        <button style="height: 35px;" aria-expanded="false" aria-haspopup="true"
+                            class="btn btn-outline-secondary dropdown-toggle" data-toggle="dropdown"
+                            type="button">Role</button>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#" wire:click="$set('filterRole', 'All')">All</a>
+                            @foreach ($roles as $roleFilter)
+                                <a class="dropdown-item" href="#"
+                                    wire:click="$set('filterRole', '{{ $roleFilter->role }}')">{{ $roleFilter->role }}</a>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class='col-2'>
+                <label for="per-page" style="font-weight: normal; margin-top: 1rem; margin-left: 1rem; cursor: pointer;"
+                    wire:click='markArchive(Object.keys(rows).filter(key => rows[key] === true))'
+                    x-show='Object.values(rows).includes(true)'>
+                    <span class="archivals btn btn-default"
+                        style="transition: color 0.3s; color: white; background-color: #0A0863; border-radius: 20px; font-size: 14px;">Mark
+                        as Archive</span>
+                </label>
+            </div>
         </div>
     </div>
     <div class="card" style="margin-left: 2rem; margin-right: 2rem;border-radius: 10px;">
-        <div wire:loading wire:target='search, filterRole, sortBy, store, update, delete, archive, unArchive, markArchive, markUnarchive, deleteSelected'>
+        <div wire:loading
+            wire:target='search, filterRole, sortBy, store, update, delete, archive, unArchive, markArchive, markUnarchive, deleteSelected'>
             <div class="overlay" style="position: absolute; width: 100%; height: 100%;">
                 <i class="fas fa-3x fa-sync-alt fa-spin"></i>
             </div>
@@ -32,7 +62,8 @@
             <table class="table table-hover" x-data="{ selectAll: false }">
                 <thead style="color: white; background-color: #7684B9; text-align: center;">
                     <tr>
-                        <th x-on:click="selectAll = !selectAll; Object.keys(rows).forEach(function(key) {rows[key] = selectAll;})">
+                        <th
+                            x-on:click="selectAll = !selectAll; Object.keys(rows).forEach(function(key) {rows[key] = selectAll;})">
                             <input type="checkbox" x-model="selectAll">
                         </th>
                         <x-table-column-header :direction="$sortField === 'id' ? $sortDirection : null" click="sortBy('id')" label='ID' sortable />
@@ -44,9 +75,11 @@
                 </thead>
                 <tbody style="text-align: center;">
                     @foreach ($users as $user)
-                        <tr @if ($my_id !== $user->id) x-bind:class="rows[{{ $user->id }}] ? 'bg-lightblue' : ''" x-init='rows[{{ $user->id }}] = false' x-on:click='rows[{{ $user->id }}] = !rows[{{ $user->id }}]' @endif>
+                        <tr
+                            @if ($my_id !== $user->id) x-bind:class="rows[{{ $user->id }}] ? 'bg-lightblue' : ''" x-init='rows[{{ $user->id }}] = false' x-on:click='rows[{{ $user->id }}] = !rows[{{ $user->id }}]' @endif>
                             <th>
-                                <input @disabled($my_id == $user->id) type="checkbox"  @if ($my_id !== $user->id) x-model="rows[{{ $user->id }}]" @endif>
+                                <input @disabled($my_id == $user->id) type="checkbox"
+                                    @if ($my_id !== $user->id) x-model="rows[{{ $user->id }}]" @endif>
                             </th>
                             <th scope="row">{{ $user->id }}</th>
                             <td>{{ $user->name }}</td>
@@ -54,21 +87,26 @@
                             <td>{{ $user->role }}</td>
                             <td>
                                 <!--VIEW PROFILE-->
-                                <button class="btn btn-primary action-btn" data-target="#view-user-btn" data-toggle="modal" title='View Account' tooltip='enable' wire:click="get_data({{ $user->id }})">
+                                <button class="btn btn-primary action-btn" data-target="#view-user-btn"
+                                    data-toggle="modal" title='View Account' tooltip='enable'
+                                    wire:click="get_data({{ $user->id }})">
                                     <i aria-hidden="true" class="fa fa-eye"></i>
                                 </button>
 
                                 @if ($my_id !== $user->id)
                                     <!--USER INFO EDIT BUTTON-->
                                     @if (in_array("Edit{$user->role}Accounts", $privileges) || in_array('EditAccounts', $privileges))
-                                        <button class="btn btn-primary action-btn" data-target="#stud-info-edit" data-toggle="modal" title='Edit Account' tooltip='enable' wire:click="get_data({{ $user->id }})">
+                                        <button class="btn btn-primary action-btn" data-target="#stud-info-edit"
+                                            data-toggle="modal" title='Edit Account' tooltip='enable'
+                                            wire:click="get_data({{ $user->id }})">
                                             <i class="fa fa-solid fa-pen"></i>
                                         </button>
                                     @endif
 
                                     {{-- ARCHIVE USER --}}
                                     @if (in_array("Archive{$user->role}Accounts", $privileges) || in_array('ArchiveAccounts', $privileges))
-                                        <button class="btn btn-primary action-btn" title='Archive Account' tooltip='enable' wire:click="archive({{ $user->id }})">
+                                        <button class="btn btn-primary action-btn" title='Archive Account'
+                                            tooltip='enable' wire:click="archive({{ $user->id }})">
                                             <i aria-hidden="true" class="fa fa-archive"></i>
                                         </button>
                                     @endif
