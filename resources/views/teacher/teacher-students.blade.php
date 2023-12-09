13838b5d4b26dae98b37d45fecfc89d7290bd720
Valerie Dela Cruz <m4nicsylla@gmail.com>
2023-12-10 02:51:04 +0700

    teacher view

--- resources/views/teacher/teacher-students.blade.php
+++ resources/views/teacher/teacher-students.blade.php
@@ -89,13 +89,13 @@
         @livewire('top-navigation-livewire')
 
         <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper" style="background-color:  rgb(253, 253, 253); padding-left: 2rem;">
+        <div class="content-wrapper pl-4" style="background-color:  rgb(253, 253, 253);">
             <!-- Content Header (Page header) -->
             <section class="content-header">
                 <div class="container-fluid">
                     <div class="row mb-2">
-                        <div class="col-sm-6" style="padding-left: 2rem; padding-top: 1rem;">
-                            <h1 style="font-weight: bold;">Students</h1>
+                        <div class="col-sm-6 pl-4 pt-2">
+                            <h1 class="font-weight-bold">Students</h1>
                         </div>
                     </div>
                 </div><!-- /.container-fluid -->
@@ -103,96 +103,15 @@
 
             <div class="row">
                 <div class="col-12">
-                    <div class="card-tools" style="display: flex; justify-content: flex-end; margin-bottom: 2rem; margin-right: 2rem;">
+                    <div class="card-tools d-flex justify-content-start mb-4 mr-4">
                         <!--SEARCH FEATURE-->
-                        <div class="input-group input-group-sm" style="max-width: 20%;">
+                        <div class="input-group input-group-sm " style="max-width: 20%;">
                             <!--SEARCH INPUT-->
                             <input class="form-control float-right" name="table_search" placeholder="Search" style="height: 35px;" type="text">
                             <div class="input-group-append">
                                 <button class="btn btn-default" data-target="#table-filter" data-toggle="modal" type="submit">
                                     <i aria-hidden="true" class="fa fa-filter"></i>
                                 </button>
-                                <!--TABLE FILTER MODAL-->
-                                <div class="modal fade" id="table-filter">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header" style="border: transparent; padding: 10px;">
-                                                <button aria-label="Close" class="close" data-dismiss="modal" type="button">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-
-                                            <form>
-                                                <div class="modal-body" style="max-height: 500px; overflow-y: auto;">
-                                                    <!--MODAL FORM TITLE-->
-                                                    <p class="card-title" style="color: #252525; font-size: 16px;">School Level</p> <br><br>
-
-                                                    <!--IMPORTANT USER DETAILS FORM SECTION-->
-                                                    <div class="row">
-                                                        <!--Junior High-->
-                                                        <div class="form-group col-sm-6" style="font-size: 14px; color: #252525;">
-                                                            <button class="btn btn-block btn-default" style="border-color: transparent; background-color: rgb(184, 184, 184); color: #252525;"> Junior High School</button>
-                                                        </div>
-                                                        <!--Senior High-->
-                                                        <div class="form-group col-sm-6" style="font-size: 14px; color: #252525;">
-                                                            <button class="btn btn-block btn-default" style="border-color: transparent; background-color: rgb(184, 184, 184); color: #252525;"> Senior High School</button>
-                                                        </div>
-                                                    </div>
-
-                                                    <div class="dropdown-divider"></div>
-
-                                                    <div class="row">
-                                                        <p style="margin-left: 5px;">Grade Level</p>
-                                                    </div>
-
-                                                    <div class="row">
-                                                        <!--Grade 7-->
-                                                        <div class="form-group col-sm-6" style="font-size: 14px; color: #252525;">
-                                                            <button class="btn btn-block btn-default" style="border-color: transparent; background-color: rgb(184, 184, 184); color: #252525;"> Grade 7</button>
-                                                        </div>
-                                                        <!--Grade 8-->
-                                                        <div class="form-group col-sm-6" style="font-size: 14px; color: #252525;">
-                                                            <button class="btn btn-block btn-default" style="border-color: transparent; background-color: rgb(184, 184, 184); color: #252525;"> Grade 8</button>
-                                                        </div>
-                                                    </div>
-
-                                                    <div class="row">
-                                                        <!--Grade 9-->
-                                                        <div class="form-group col-sm-6" style="font-size: 14px; color: #252525;">
-                                                            <button class="btn btn-block btn-default" style="border-color: transparent; background-color: rgb(184, 184, 184); color: #252525;"> Grade 9</button>
-                                                        </div>
-                                                        <!--Grade 10-->
-                                                        <div class="form-group col-sm-6" style="font-size: 14px; color: #252525;">
-                                                            <button class="btn btn-block btn-default" style="border-color: transparent; background-color: rgb(184, 184, 184); color: #252525;"> Grade 10</button>
-                                                        </div>
-                                                    </div>
-
-                                                    <div class="row">
-                                                        <!--Grade 11-->
-                                                        <div class="form-group col-sm-6" style="font-size: 14px; color: #252525;">
-                                                            <button class="btn btn-block btn-default" style="border-color: transparent; background-color: rgb(184, 184, 184); color: #252525;"> Grade 11</button>
-                                                        </div>
-                                                        <!--Grade 12-->
-                                                        <div class="form-group col-sm-6" style="font-size: 14px; color: #252525;">
-                                                            <button class="btn btn-block btn-default" style="border-color: transparent; background-color: rgb(184, 184, 184); color: #252525;"> Grade 12</button>
-                                                        </div>
-                                                    </div>
-
-                                                    <div class="row">
-                                                        <!--RESET-->
-                                                        <div class="form-group col-sm-6">
-                                                            <button class="btn btn-block btn-default" style="border-color: transparent; background-color: #d9d9f3; color: #0A0863;"> Reset</button>
-                                                        </div>
-                                                        <!--DONE-->
-                                                        <div class="form-group col-sm-6">
-                                                            <button class="btn btn-block btn-default" style="border-color: transparent; background-color: #0A0863; color: #252525; color:white;">Done</button>
-                                                        </div>
-                                                    </div>
-                                                </div> <!-- /.modal-body -->
-                                            </form>
-                                        </div><!-- /.modal-content -->
-                                    </div><!-- /.modal-dialog -->
-                                </div><!-- /.modal-end -->
                             </div><!-- /.input-group-append-->
                         </div><!-- /.input-group -->
                     </div><!-- /.card-tools-->
@@ -393,6 +312,88 @@
             </div><!-- /pagination -->
         </div><!-- /.content-wrapper -->
     </div><!-- /.wrapper -->
+
+    <!--TABLE FILTER MODAL-->
+    <div class="modal fade" id="table-filter">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header" style="border: transparent; padding: 10px;">
+                    <button aria-label="Close" class="close" data-dismiss="modal" type="button">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form>
+                    <div class="modal-body" style="max-height: 500px; overflow-y: auto;">
+                        <!--MODAL FORM TITLE-->
+                        <p class="card-title" style="color: #252525; font-size: 16px;">School Level</p> <br><br>
+
+                        <!--IMPORTANT USER DETAILS FORM SECTION-->
+                        <div class="row">
+                            <!--Junior High-->
+                            <div class="form-group col-sm-6" style="font-size: 14px; color: #252525;">
+                                <button class="btn btn-block btn-default" style="border-color: transparent; background-color: rgb(184, 184, 184); color: #252525;"> Junior High School</button>
+                            </div>
+                            <!--Senior High-->
+                            <div class="form-group col-sm-6" style="font-size: 14px; color: #252525;">
+                                <button class="btn btn-block btn-default" style="border-color: transparent; background-color: rgb(184, 184, 184); color: #252525;"> Senior High School</button>
+                            </div>
+                        </div>
+
+                        <div class="dropdown-divider"></div>
+
+                        <div class="row">
+                            <p style="margin-left: 5px;">Grade Level</p>
+                        </div>
+
+                        <div class="row">
+                            <!--Grade 7-->
+                            <div class="form-group col-sm-6" style="font-size: 14px; color: #252525;">
+                                <button class="btn btn-block btn-default" style="border-color: transparent; background-color: rgb(184, 184, 184); color: #252525;"> Grade 7</button>
+                            </div>
+                            <!--Grade 8-->
+                            <div class="form-group col-sm-6" style="font-size: 14px; color: #252525;">
+                                <button class="btn btn-block btn-default" style="border-color: transparent; background-color: rgb(184, 184, 184); color: #252525;"> Grade 8</button>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <!--Grade 9-->
+                            <div class="form-group col-sm-6" style="font-size: 14px; color: #252525;">
+                                <button class="btn btn-block btn-default" style="border-color: transparent; background-color: rgb(184, 184, 184); color: #252525;"> Grade 9</button>
+                            </div>
+                            <!--Grade 10-->
+                            <div class="form-group col-sm-6" style="font-size: 14px; color: #252525;">
+                                <button class="btn btn-block btn-default" style="border-color: transparent; background-color: rgb(184, 184, 184); color: #252525;"> Grade 10</button>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <!--Grade 11-->
+                            <div class="form-group col-sm-6" style="font-size: 14px; color: #252525;">
+                                <button class="btn btn-block btn-default" style="border-color: transparent; background-color: rgb(184, 184, 184); color: #252525;"> Grade 11</button>
+                            </div>
+                            <!--Grade 12-->
+                            <div class="form-group col-sm-6" style="font-size: 14px; color: #252525;">
+                                <button class="btn btn-block btn-default" style="border-color: transparent; background-color: rgb(184, 184, 184); color: #252525;"> Grade 12</button>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <!--RESET-->
+                            <div class="form-group col-sm-6">
+                                <button class="btn btn-block btn-default" style="border-color: transparent; background-color: #d9d9f3; color: #0A0863;"> Reset</button>
+                            </div>
+                            <!--DONE-->
+                            <div class="form-group col-sm-6">
+                                <button class="btn btn-block btn-default" style="border-color: transparent; background-color: #0A0863; color: #252525; color:white;">Done</button>
+                            </div>
+                        </div>
+                    </div> <!-- /.modal-body -->
+                </form>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal-end -->
 
     <footer class="main-footer">
         <div class="float-right d-none d-sm-block">
