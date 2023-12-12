0212fa72e2e8ab3592aa40a12096dc2874e35bc6
Kimwel Beller <94892701+Kheemwel@users.noreply.github.com>
2023-12-12 19:23:10 +0800

    fix adding principal account

--- resources/views/livewire/user_accounts/add-principal.blade.php
+++ resources/views/livewire/user_accounts/add-principal.blade.php
@@ -1,8 +1,8 @@
 <div style="display: flex; flex-direction: column;">
     <p class="card-title" style="font-size: 14px; margin-bottom: 1rem;">Position</p>
     <!--ROLE DROPDOWN BUTTON-->
-    <div class="input-group-prepend">
-        <select required class="form-select form-select-sm mb-2" id="roles" required selected wire:model.live.debounce.500ms="principal_position">
+    <div class="input-group-prepend" wire:ignore>
+        <select required class="form-select form-select-sm mb-2" id="roles" required selected wire:model="principal_position">
             @if ($principal_position == '')
                 <option selected>Select Position</option>
             @endif
@@ -10,8 +10,8 @@
                 <option value="{{ $position->position }}">{{ $position->position }}</option>
             @endforeach
         </select>
-        @error('principal_position')
-            <span class="text-danger">You must select a position for this principal</span>
-        @enderror
     </div>
+    @error('principal_position')
+        <span class="text-danger">You must select a position for this principal</span>
+    @enderror
 </div>
