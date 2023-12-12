3a6bb72ba442885971a575c416cab540c1cc20c0
Valerie Dela Cruz <m4nicsylla@gmail.com>
2023-12-12 19:10:13 +0700

    User Accounts

--- resources/views/livewire/user_accounts/accounts-table.blade.php
+++ resources/views/livewire/user_accounts/accounts-table.blade.php
@@ -1,8 +1,9 @@
 <div x-data="activeAccounts">
-    <div style="margin-left: 2rem; margin-right: 2rem;">
-        <div class="row" style="margin-top: 1rem;">
-            <div class="col-5">
-                <div class="input-group input-group-sm" style="width: 70%;">
+    <div class="ml-2 mr-2">
+        <div class="row mt-1">
+            <!-- Search Input -->
+            <div class="col-md-5 col-sm-12 d-flex align-items-center mb-2">
+                <div class="input-group input-group-sm" style="width: 100%;">
                     <input class="form-control float-right" id="searchActiveAccounts" name="table_search" placeholder="Search" style="height: 35px;" type="text" x-model='search'>
                     <div class="input-group-append">
                         <button aria-expanded="false" aria-haspopup="true" class="btn btn-outline-secondary dropdown-toggle" data-toggle="dropdown" style="height: 35px;" type="button" x-text="filterRole == '' ? 'Role' : filterRole">Role</button>
@@ -15,43 +16,49 @@
                     </div>
                 </div>
             </div>
-
-            <div class="col-6 d-flex justify-content-end ml-auto">
-                <!--ADD USER BUTTON-->
-                @if (wordsExistInArray(['Add', 'Account'], $privileges))
-                    <button class="btn btn-default" data-target="#addUserModal" data-toggle="modal" style="border-radius: 10px; font-size: 12px; margin-left: 1rem; background-color: #0A0863; color: white;" type="button">
-                        <i aria-hidden="true" class="fa fa-plus"></i>
-                        Add User
-                    </button>
-                    <!--BATCH ADD USER BUTTON-->
-                    <button class="btn btn-default" data-target="#importUsersModal" data-toggle="modal" style="border-radius: 10px; font-size: 12px; margin-left: 1rem; background-color: #0A0863; color: white;" type="button">
-                        <i class="fa fa-solid fa-file-import" style="color: white;"></i>
-                        Import User Accounts
-                    </button>
-                @endif
-                <!--DOWNLOAD TABLE BUTTON-->
-                @if (wordsExistInArray(['Export', 'Account'], $privileges))
-                    <button class="btn btn-default" style="border-radius: 10px; font-size: 12px; margin-left: 1rem; background-color: #0A0863; color: white;" type="button" wire:click='export()'>
-                        <i class="fa fa-solid fa-file-export" style="color: white;"></i>
-                        Export User Accounts
-                    </button>
-                @endif
+        
+            <!-- Buttons -->
+            <div class="col-md-7 col-sm-12 mb-3 mt-3">
+                <!-- Add User and Batch Add User Buttons -->
+                <div class="row justify-content-end">
+                    @if (wordsExistInArray(['Add', 'Account'], $privileges))
+                        <div class="col-lg-2 col-md-4 col-sm-12 mb-2">
+                            <button class="btn btn-default text-xs mb-1 mb-md-0" data-target="#addUserModal" data-toggle="modal" style="width: 100%; border-radius: 10px; background-color: #0A0863; color: white;" type="button">
+                                <i aria-hidden="true" class="fa fa-plus"></i> Add User
+                            </button>
+                        </div>
+                        <div class="col-lg-2 col-md-4 col-sm-12 mb-2">
+                            <button class="btn btn-default text-xs mb-1 mb-md-0" data-target="#importUsersModal" data-toggle="modal" style="width: 100%; border-radius: 10px; background-color: #0A0863; color: white;" type="button">
+                                <i class="fa fa-solid fa-file-import" style="color: white;"></i> Import
+                            </button>
+                        </div>
+                    @endif
+                    
+                    <!-- Download Table Button -->
+                    <div class="col-lg-2 col-md-4 col-sm-12 mb-2">
+                        @if (wordsExistInArray(['Export', 'Account'], $privileges))
+                            <button class="btn btn-default text-xs" style="width: 100%; border-radius: 10px; background-color: #0A0863; color: white;" type="button" wire:click='export()'>
+                                <i class="fa fa-solid fa-file-export" style="color: white;"></i> Export
+                            </button>
+                        @endif
+                    </div>
+                </div>
             </div>
         </div>
+        
 
-        <div class="row" style="margin-top: 1rem;">
-            <div class="col-3 d-flex justify-content-start">
-                <label class="m-0" for="per-page" style="padding-top: 10px; font-weight: normal; margin-top: 1rem; margin-left: 1rem; cursor: pointer;" wire:click='markArchive(getSelectedRows())' x-cloak x-show='showAction()'>
-                    <span class="archivals btn btn-default" style="transition: color 0.3s; color: white; background-color: #0A0863; border-radius: 10px; font-size: 14px;">Mark
-                        as Archive</span>
+        <div class="row mt-1">
+            <div class="col-md-3 col-sm-12 d-flex justify-content-start mb-2">
+                <label class="m-0 font-weight-normal mt-1 ml-1" for="per-page" style="cursor: pointer;" wire:click='markArchive(getSelectedRows())' x-cloak x-show='showAction()'>
+                    <span class="archivals btn btn-default text-xs" style="width: 100%; transition: color 0.3s; color: white; background-color: #0A0863; border-radius: 10px;">Mark as Archive</span>
                 </label>
             </div>
 
-            <div class="col-6 d-flex justify-content-end ml-auto">
-                <label for="per-page" style="font-weight: normal; margin-top: 1rem;">
+            <div class="col-md-6 col-sm-12 d-flex justify-content-end align-items-center ml-auto">
+                <label class="font-weight-normal mt-1" for="per-page">
                     Show
                     <span>
-                        <select class="form-select form-select-sm mb-2" id='per-page' x-model="perPage">
+                        <select class="form-select form-select-sm mb-2 text-center" id='per-page' x-model="perPage">
                             <option selected>5</option>
                             <option>10</option>
                             <option>15</option>
@@ -67,7 +74,8 @@
             </div>
         </div>
     </div>
-    <div class="card" style="margin-left: 2rem; margin-right: 2rem;border-radius: 10px;">
+    
+    <div class="card ml-2 mr-2" style="border-radius: 10px;">
         <div wire:loading wire:target='markArchive, resetInputFields'>
             <div class="overlay" style="position: absolute; width: 100%; height: 100%;">
                 <i class="fas fa-3x fa-sync-alt fa-spin"></i>
@@ -79,7 +87,7 @@
             <!-- DataTable -->
             <table class="table table-hover">
                 <!-- Table Header -->
-                <thead style="color: white; background-color: #7684B9; text-align: center;">
+                <thead class="text-center" style="color: white; background-color: #7684B9;">
                     <tr>
                         <th x-on:click="checkAll()">
                             <input type="checkbox" x-model="selectAll">
@@ -93,7 +101,7 @@
                 </thead>
 
                 <!-- Table Body -->
-                <tbody style="text-align: center;" x-data="{ myID: {{ $my_id }} }">
+                <tbody class="text-center" x-data="{ myID: {{ $my_id }} }">
                     <template x-for="user in filteredData">
                         <tr x-bind:class="myID === user.id ? 'font-weight-bold' : ''">
                             <template x-if="myID !== user.id">
@@ -126,21 +134,28 @@
         <!-- /.card-body -->
     </div>
     <!-- Pagination Controls -->
-    <div class="row">
-        <div class="col-6 font-weight-bold">
+    <div class="row mt-2">
+        <div class="col-12 font-weight-bold text-center text-md-left">
             <p x-text="showedEntries"></p>
         </div>
-        <div class="col-6 d-flex justify-content-end text-center">
-            <button class="col-1" x-bind:disabled="currentPage == 1" x-on:click="currentPage = 1">
-                << </button>
-                    <button class="col-1" x-bind:disabled="currentPage == 1" x-on:click="prevPage()">
-                        < </button>
-                            <template x-for="item in pages">
-                                <button class="col-1 button button-defalut" x-bind:class="currentPage == item ? 'bg-blue' : ''" x-on:click="currentPage = item" x-text="item"></button>
-                            </template>
-                            <button class="col-1 paginate_button page-item" x-bind:disabled="currentPage >= totalPages" x-on:click="nextPage()"> > </button>
-                            <button class="col-1" x-bind:disabled="currentPage >= totalPages" x-on:click.prevent="currentPage = totalPages"> >> </button>
+        
+        <div class="col-12 d-md-flex justify-content-md-end justify-content-center text-center">
+            <button class="btn btn-sm btn-light text-dark mx-1 font-weight-bold" x-bind:disabled="currentPage == 1" x-on:click="currentPage = 1">
+                &lt;&lt;
+            </button>
+            <button class="btn btn-sm btn-light text-dark mx-1 font-weight-bold" x-bind:disabled="currentPage == 1" x-on:click="prevPage()">
+                &lt;
+            </button>
+            <template x-for="item in pages">
+                <button class="btn btn-sm mx-1" style="border-radius: 50%;" x-bind:class="{'bg-blue': currentPage == item, 'btn-primary text-white': currentPage == item, 'btn-light text-dark': currentPage != item}" x-on:click="currentPage = item" x-text="item"></button>
+            </template>
+            <button class="btn btn-sm btn-light text-dark mx-1 font-weight-bold" x-bind:disabled="currentPage >= totalPages" x-on:click="nextPage()">
+                &gt;
+            </button>
+            <button class="btn btn-sm btn-light text-dark mx-1 font-weight-bold" x-bind:disabled="currentPage >= totalPages" x-on:click.prevent="currentPage = totalPages">
+                &gt;&gt;
+            </button>
         </div>
     </div>
-
+    
 </div>
