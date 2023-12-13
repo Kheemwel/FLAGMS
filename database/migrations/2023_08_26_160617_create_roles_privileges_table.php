c4f5da76466ed74af196a64fac489f005b51a91f
Kimwel Beller <94892701+Kheemwel@users.noreply.github.com>
2023-12-13 11:55:15 +0800

    update guidance privileges - guidance are also now the superadmin

--- database/migrations/2023_08_26_160617_create_roles_privileges_table.php
+++ database/migrations/2023_08_26_160617_create_roles_privileges_table.php
@@ -25,7 +25,8 @@ return new class extends Migration
             'role' => 'Guidance',
             'is_default' => true
         ]);
-        $guidance->privileges()->attach([3, 4, 6, 8, 12, 14, 17, 20, 22, 25, 28, 30, 34, 36, 37, 39, 48, 52, 53, 55, 56, 57, 58, 59, 60]);
+        $guidance->privileges()->attach([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+        27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 42, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]);
      
         $student = Roles::create([
             'role' => 'Student',
