87c323cd70bf89e6aeec772a4b3f92d5d89f9d54
Valerie Dela Cruz <m4nicsylla@gmail.com>
2024-01-07 19:32:58 +0700

    audit trail and acknowledgement ui enhanced

--- resources/views/livewire/fill_out_forms/submit-home-visitation-form.blade.php
+++ resources/views/livewire/fill_out_forms/submit-home-visitation-form.blade.php
@@ -1,14 +1,14 @@
 <!-- Modal -->
 <div aria-hidden="true" aria-labelledby="modelTitleId" class="modal fade" id="confirm-home" role="dialog" tabindex="-1" wire:ignore.self>
     <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content" x-data="{ agree: false}">
+        <div class="modal-content" x-data="{ agree: false}" style="border-radius: 10px">
             <div class="modal-header">
                 <h5 class="modal-title font-weight-bold" style="color: #0A0863;">ACKNOWLEDGEMENT OF FORM CONTENT</h5>
                 <button aria-label="Close" class="close" data-dismiss="modal" type="button">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body">
+            <div class="modal-body text-justify mr-1 ml-1">
                 <div class="form-check">
                     <label class="form-check-label">
                         <input x-model="agree" class="form-check-input" id="" name="" type="checkbox" value="checkedValue">
@@ -17,7 +17,7 @@
                 </div>
             </div>
             <div class="modal-footer justify-content-center">
-                <button data-dismiss="modal" x-bind:disabled="!agree" class="btn btn-primary" style="width: 300px; margin-left: 5px; border-color: #0A0863; background-color: #0A0863; color: white; font-size: 14px;" type="submit" wire:click='updateHomeVisitationForm()'>Confirm and Submit</button>
+                <button data-dismiss="modal" x-bind:disabled="!agree" class="btn btn-primary" style="width: 100%; border-color: #0A0863; background-color: #0A0863; color: white; font-size: 14px;" type="submit" wire:click='updateHomeVisitationForm()'>Confirm and Submit</button>
             </div>
         </div>
     </div>
